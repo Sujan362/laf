9439fc3cfb211bc360a2c6b39599f1dab51bb040
Sujan362 <u5918127@au.edu>
2020-10-10 15:54:05 +0700

    Updates

---
+++ precache-manifest.b7191bfebd6a81c3e31914fd5babaf36.js
@@ -0,0 +1,458 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f9aaa4b5ae90dc4001041d8f56fb76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b1404de403cb1cd3e6a",
+    "url": "/static/css/main.97b86c0a.chunk.css"
+  },
+  {
+    "revision": "046128b0cc341cd340c6",
+    "url": "/static/js/2.ac977919.chunk.js"
+  },
+  {
+    "revision": "e2df5b3323a316e555e569ab3f246bbe",
+    "url": "/static/js/2.ac977919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1404de403cb1cd3e6a",
+    "url": "/static/js/main.cb2ee05a.chunk.js"
+  },
+  {
+    "revision": "37bd701053505905302a",
+    "url": "/static/js/runtime-main.5e250fb4.js"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/1597753744273add.62b40844.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/1597753761215shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/1597754361798add.62b40844.jpg"
+  },
+  {
+    "revision": "bc6299bd9e09bc43026fff8a9a08f0c1",
+    "url": "/static/media/1597821564716steam.bc6299bd.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/1597829166435shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/1597829445481shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/1597829473093shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/1597829848049add.62b40844.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/1597829940650add.62b40844.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/1597830144689add.62b40844.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/1597830170349add.62b40844.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/1597835363205shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/1597835384629shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "75276928a088f2a6b690659912ead68f",
+    "url": "/static/media/1597841640326abstract_girl_artwork_hd_wallpaper_by_matipatloko-d8m8mof (1).75276928.jpg"
+  },
+  {
+    "revision": "5dcb5f9f9f0e703d182f9eff8094b4b3",
+    "url": "/static/media/1597841675612android-robot-scifi-concept-art-wallpaper.5dcb5f9f.jpeg"
+  },
+  {
+    "revision": "66307aecae29b541a5a1d66e732b6ff3",
+    "url": "/static/media/1598782026251Eagles-Digital-Artwork-8_www.FullHDWpp.com_.66307aec.jpg"
+  },
+  {
+    "revision": "8ff3007ddc526fc371ffec35492a61bd",
+    "url": "/static/media/1598964890339Anime-HD-Wallpapers-Free-Download-For-Desktop-Wallpaperxyz.com-29.8ff3007d.jpg"
+  },
+  {
+    "revision": "c32f0dd74b295a6f881a316768efd019",
+    "url": "/static/media/1598964952252artwork-1518514182865-7429.c32f0dd7.jpg"
+  },
+  {
+    "revision": "8ff3007ddc526fc371ffec35492a61bd",
+    "url": "/static/media/1599049171821Anime-HD-Wallpapers-Free-Download-For-Desktop-Wallpaperxyz.com-29.8ff3007d.jpg"
+  },
+  {
+    "revision": "75276928a088f2a6b690659912ead68f",
+    "url": "/static/media/1599049618114abstract_girl_artwork_hd_wallpaper_by_matipatloko-d8m8mof.75276928.jpg"
+  },
+  {
+    "revision": "be5b8e13412496946e3fd4a005b7ac59",
+    "url": "/static/media/1599051755929Screenshot (2).be5b8e13.png"
+  },
+  {
+    "revision": "25a2b4c7db74aa7a25a2cc46cd3836d0",
+    "url": "/static/media/1599385460495Screenshot (1).25a2b4c7.png"
+  },
+  {
+    "revision": "25a2b4c7db74aa7a25a2cc46cd3836d0",
+    "url": "/static/media/1599385801804Screenshot (1).25a2b4c7.png"
+  },
+  {
+    "revision": "34d9b83c84ea5315c317352aeac5d94a",
+    "url": "/static/media/1599387795638ixs.34d9b83c.jpg"
+  },
+  {
+    "revision": "259ad1a6e1ac220576660ca628ce330e",
+    "url": "/static/media/1599387975237pen.259ad1a6.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/15998215863591597753761215shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "8ff3007ddc526fc371ffec35492a61bd",
+    "url": "/static/media/15998224181501598964890339Anime-HD-Wallpapers-Free-Download-For-Desktop-Wallpaperxyz.com-29.8ff3007d.jpg"
+  },
+  {
+    "revision": "259ad1a6e1ac220576660ca628ce330e",
+    "url": "/static/media/1599825033831pen.259ad1a6.jpg"
+  },
+  {
+    "revision": "c6282848a86c1473c4cdc3626ca2c60f",
+    "url": "/static/media/1599826986366pendrive.c6282848.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/15998295501801597753761215shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "8ff3007ddc526fc371ffec35492a61bd",
+    "url": "/static/media/160008003057015998224181501598964890339Anime-HD-Wallpapers-Free-Download-For-Desktop-Wallpaperxyz.com-29.8ff3007d.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16000822143671597829848049add.62b40844.jpg"
+  },
+  {
+    "revision": "2aa2a4440bf5e04aea89916192602e46",
+    "url": "/static/media/1600083539215note10.2aa2a444.jpg"
+  },
+  {
+    "revision": "a82773aed3114a181d99e7283e412fa0",
+    "url": "/static/media/1600083590403wallet.a82773ae.jpg"
+  },
+  {
+    "revision": "c6282848a86c1473c4cdc3626ca2c60f",
+    "url": "/static/media/16004230781481599826986366pendrive.c6282848.jpg"
+  },
+  {
+    "revision": "a82773aed3114a181d99e7283e412fa0",
+    "url": "/static/media/16005048817201600083590403wallet.a82773ae.jpg"
+  },
+  {
+    "revision": "c32f0dd74b295a6f881a316768efd019",
+    "url": "/static/media/16005050508561598964952252artwork-1518514182865-7429.c32f0dd7.jpg"
+  },
+  {
+    "revision": "34d9b83c84ea5315c317352aeac5d94a",
+    "url": "/static/media/16005055352141599387795638ixs.34d9b83c.jpg"
+  },
+  {
+    "revision": "75276928a088f2a6b690659912ead68f",
+    "url": "/static/media/16005079456161597841640326abstract_girl_artwork_hd_wallpaper_by_matipatloko-d8m8mof (1).75276928.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/16005119496821597753761215shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16005119938561597829940650add.62b40844.jpg"
+  },
+  {
+    "revision": "5dcb5f9f9f0e703d182f9eff8094b4b3",
+    "url": "/static/media/16014614502061597841675612android-robot-scifi-concept-art-wallpaper.5dcb5f9f.jpeg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/16014615463881597829473093shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16014633958861597753744273add.62b40844.jpg"
+  },
+  {
+    "revision": "2ffbdea92d9feee973f18adc987f621d",
+    "url": "/static/media/1601545068163cardImage.2ffbdea9.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/16017194416671597753761215shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16017197170121597829848049add.62b40844.jpg"
+  },
+  {
+    "revision": "75276928a088f2a6b690659912ead68f",
+    "url": "/static/media/16017197567631597841640326abstract_girl_artwork_hd_wallpaper_by_matipatloko-d8m8mof (1).75276928.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16017253968641597830144689add.62b40844.jpg"
+  },
+  {
+    "revision": "34d9b83c84ea5315c317352aeac5d94a",
+    "url": "/static/media/16017986723971599387795638ixs.34d9b83c.jpg"
+  },
+  {
+    "revision": "5dcb5f9f9f0e703d182f9eff8094b4b3",
+    "url": "/static/media/16017986862031597841675612android-robot-scifi-concept-art-wallpaper.5dcb5f9f.jpeg"
+  },
+  {
+    "revision": "75276928a088f2a6b690659912ead68f",
+    "url": "/static/media/16018044505541597841640326abstract_girl_artwork_hd_wallpaper_by_matipatloko-d8m8mof (1).75276928.jpg"
+  },
+  {
+    "revision": "8ff3007ddc526fc371ffec35492a61bd",
+    "url": "/static/media/16018050168491598964890339Anime-HD-Wallpapers-Free-Download-For-Desktop-Wallpaperxyz.com-29.8ff3007d.jpg"
+  },
+  {
+    "revision": "c32f0dd74b295a6f881a316768efd019",
+    "url": "/static/media/16018050522891598964952252artwork-1518514182865-7429.c32f0dd7.jpg"
+  },
+  {
+    "revision": "75276928a088f2a6b690659912ead68f",
+    "url": "/static/media/16018055171621599049618114abstract_girl_artwork_hd_wallpaper_by_matipatloko-d8m8mof.75276928.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/16018055998491597829473093shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "75276928a088f2a6b690659912ead68f",
+    "url": "/static/media/16018056133931597841640326abstract_girl_artwork_hd_wallpaper_by_matipatloko-d8m8mof (1).75276928.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/16018058512651597829473093shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16019765218061597754361798add.62b40844.jpg"
+  },
+  {
+    "revision": "25a2b4c7db74aa7a25a2cc46cd3836d0",
+    "url": "/static/media/16019782425011599385460495Screenshot (1).25a2b4c7.png"
+  },
+  {
+    "revision": "75276928a088f2a6b690659912ead68f",
+    "url": "/static/media/16019783721001599049618114abstract_girl_artwork_hd_wallpaper_by_matipatloko-d8m8mof.75276928.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16019785762131597830144689add.62b40844.jpg"
+  },
+  {
+    "revision": "5dcb5f9f9f0e703d182f9eff8094b4b3",
+    "url": "/static/media/16019787123031597841675612android-robot-scifi-concept-art-wallpaper.5dcb5f9f.jpeg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16019801720281597829848049add.62b40844.jpg"
+  },
+  {
+    "revision": "701817390006cdac1eb96c6f47b027a6",
+    "url": "/static/media/1602237243378david-beckham-pompadour.70181739.jpg"
+  },
+  {
+    "revision": "980414c3c9c95c18e15a32d3516abe8b",
+    "url": "/static/media/1602238901719ipad.980414c3.jpg"
+  },
+  {
+    "revision": "a82773aed3114a181d99e7283e412fa0",
+    "url": "/static/media/1602238996967wallet.a82773ae.jpg"
+  },
+  {
+    "revision": "c8150e0709199a44d5e6033b76fb0898",
+    "url": "/static/media/1602239154218calculator.c8150e07.jpg"
+  },
+  {
+    "revision": "c6282848a86c1473c4cdc3626ca2c60f",
+    "url": "/static/media/1602239324932pendrive.c6282848.jpg"
+  },
+  {
+    "revision": "1c83679ce6a897898b0c94231c77be86",
+    "url": "/static/media/1602244149670ipromax.1c83679c.jpg"
+  },
+  {
+    "revision": "259ad1a6e1ac220576660ca628ce330e",
+    "url": "/static/media/1602244305144pen.259ad1a6.jpg"
+  },
+  {
+    "revision": "2aa2a4440bf5e04aea89916192602e46",
+    "url": "/static/media/1602244543520note10.2aa2a444.jpg"
+  },
+  {
+    "revision": "8f5fd9798dd74ab3846374af75463ec1",
+    "url": "/static/media/1602246509220Lcase.8f5fd979.jpg"
+  },
+  {
+    "revision": "127fcbef7397cb8e95e705feafc54350",
+    "url": "/static/media/1602246909348charger.127fcbef.jpg"
+  },
+  {
+    "revision": "b3425dd9a9bdff524e8d292feec590eb",
+    "url": "/static/media/1602247088913oneplus-8-pro.b3425dd9.jpg"
+  },
+  {
+    "revision": "2aa2a4440bf5e04aea89916192602e46",
+    "url": "/static/media/1602247555596note10.2aa2a444.jpg"
+  },
+  {
+    "revision": "b3425dd9a9bdff524e8d292feec590eb",
+    "url": "/static/media/1602247896203oneplus-8-pro.b3425dd9.jpg"
+  },
+  {
+    "revision": "259ad1a6e1ac220576660ca628ce330e",
+    "url": "/static/media/1602248596967pen.259ad1a6.jpg"
+  },
+  {
+    "revision": "a82773aed3114a181d99e7283e412fa0",
+    "url": "/static/media/1602248636223wallet.a82773ae.jpg"
+  },
+  {
+    "revision": "2aa2a4440bf5e04aea89916192602e46",
+    "url": "/static/media/1602249859750note10.2aa2a444.jpg"
+  },
+  {
+    "revision": "f6fc593d4af209ebed71a9eb2b2c2d3d",
+    "url": "/static/media/16022499440202.f6fc593d.jpg"
+  },
+  {
+    "revision": "34d9b83c84ea5315c317352aeac5d94a",
+    "url": "/static/media/1602250213040ixs.34d9b83c.jpg"
+  },
+  {
+    "revision": "62b40844f787a3b70ba9d5bee1bf2d14",
+    "url": "/static/media/16022502899541597753744273add.62b40844.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/16022506762901597753761215shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "06b81ef293c4bace62f81c6345c9f858",
+    "url": "/static/media/16022511465761.06b81ef2.jpg"
+  },
+  {
+    "revision": "06b81ef293c4bace62f81c6345c9f858",
+    "url": "/static/media/16022513453921.06b81ef2.jpg"
+  },
+  {
+    "revision": "a82773aed3114a181d99e7283e412fa0",
+    "url": "/static/media/1602251470699wallet.a82773ae.jpg"
+  },
+  {
+    "revision": "b3425dd9a9bdff524e8d292feec590eb",
+    "url": "/static/media/1602251517666oneplus-8-pro.b3425dd9.jpg"
+  },
+  {
+    "revision": "06b81ef293c4bace62f81c6345c9f858",
+    "url": "/static/media/16022516127381.06b81ef2.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/NaN.f683c57f.bin"
+  },
+  {
+    "revision": "82001e70da3ace3e31f854a7746312ec",
+    "url": "/static/media/avatar.82001e70.jpg"
+  },
+  {
+    "revision": "c6ddfd4781730d530a8ff8c905eae48e",
+    "url": "/static/media/bell.c6ddfd47.mp3"
+  },
+  {
+    "revision": "c8150e0709199a44d5e6033b76fb0898",
+    "url": "/static/media/calc.c8150e07.jpg"
+  },
+  {
+    "revision": "c8150e0709199a44d5e6033b76fb0898",
+    "url": "/static/media/calculator.c8150e07.jpg"
+  },
+  {
+    "revision": "2ffbdea92d9feee973f18adc987f621d",
+    "url": "/static/media/cardImage.2ffbdea9.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/fuckery.f683c57f.bin"
+  },
+  {
+    "revision": "34d9b83c84ea5315c317352aeac5d94a",
+    "url": "/static/media/ixs.34d9b83c.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/newname.f683c57f.bin"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/newname.f683c57f.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/newnamejpg.f683c57f.bin"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/newnameshopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "2aa2a4440bf5e04aea89916192602e46",
+    "url": "/static/media/note10.2aa2a444.jpg"
+  },
+  {
+    "revision": "259ad1a6e1ac220576660ca628ce330e",
+    "url": "/static/media/pen.259ad1a6.jpg"
+  },
+  {
+    "revision": "c6282848a86c1473c4cdc3626ca2c60f",
+    "url": "/static/media/pendrive.c6282848.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/shopping-woman.f683c57f.jpg"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/something.f683c57f.bin"
+  },
+  {
+    "revision": "f683c57fe2718f4046c5d6c5ad699880",
+    "url": "/static/media/something.f683c57f.jpg"
+  },
+  {
+    "revision": "a82773aed3114a181d99e7283e412fa0",
+    "url": "/static/media/wallet.a82773ae.jpg"
+  }
+]);
